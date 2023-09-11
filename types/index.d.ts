df9f150279358b558f5b82cbf8550d010bfac0a3
Danny Rorabaugh <imnasnainaec@gmail.com>
2023-09-11 14:38:27 -0400

    Make promise

--- types/index.d.ts
+++ types/index.d.ts
@@ -61,7 +61,7 @@ export interface MaterialTableProps<RowData extends object> {
   options?: Options<RowData>;
   parentChildData?: (row: RowData, rows: RowData[]) => RowData | undefined;
   localization?: Localization;
-  onRowsPerPageChange?: (pageSize: number) => void;
+  onRowsPerPageChange?: (pageSize: number) => Promise<unknown>;
   onPageChange?: (page: number, pageSize: number) => void;
   onChangeColumnHidden?: (column: Column<RowData>, hidden: boolean) => void;
   onColumnDragged?: (sourceIndex: number, destinationIndex: number) => void;
